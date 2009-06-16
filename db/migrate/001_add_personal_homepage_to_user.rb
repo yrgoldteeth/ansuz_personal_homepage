762d7f03fdfcf379fed96633800ec0da4ac51c6e
Nicholas Fine <nicholas.fine@gmail.com>
2009-06-16 15:18:58 -0500

    removed unneeded migration

--- db/migrate/001_add_personal_homepage_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddPersonalHomePageToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :personal_homepage, :boolean
-  end
-
-  def self.down
-    remove_column :users, :personal_homepage
-  end
-end
